664657112076c75419b22cc37247cd78c665307f
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-13 15:10:09 -0400

    added debug text to rulers and re-installed gem rulers

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -2,7 +2,8 @@ require "rulers/version"
 
 module Rulers
   class Application
-  	def call(evn)
+  	def call(env)
+  		`echo debug > debug.txt`
   		[200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers"]]
   	end
   end
